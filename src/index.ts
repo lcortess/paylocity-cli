68d269b8bb734d37339f7754c9828ee779be0197
Jose Luis <jose@lcortes.net>
2020-01-09 00:51:02 -0600

    print pace table

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import { Timesheet } from './models/timesheet';
+import { Pace } from './views/Pace';
 
 const time: Timesheet = new Timesheet();
 time
   .start()
   .then(() => {
-    console.log(time.totalHours, time.today.getClockInHour(), time.today.getCurrentHours());
+    console.log('ClockIn: ', time.today.getClockInHour());
+    console.log('Total hours: ', time.totalHours);
+    console.log('Today Hours: ', time.today.getCurrentHours());
+
+    const pace = new Pace(time.totalHours, time.today.getCurrentHours());
+    console.log(pace.getTable());
+
     time.closePage();
   })
   .catch(error => {
