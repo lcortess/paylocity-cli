1ee152b1f2b2aef32513abca7e50625211290c15
Jose Luis <jose@lcortes.net>
2020-01-22 23:58:49 -0600

    support more than 1 clock in /out by day

--- src/models/today.ts
+++ src/models/today.ts
@@ -2,31 +2,77 @@ import * as moment from 'moment';
 import { Hour } from './hour';
 
 export class Today {
-  private currentHours: number;
-  private clockIn: moment.Moment;
-  private date: Date = new Date();
+  private clockIn: moment.Moment[];
+  private clockOut: Array<moment.Moment | null> = [null];
 
-  constructor(clockIn: string) {
-    this.clockIn = moment();
-    this.setClockInHour(clockIn);
-    this.currentHours = this.getHoursToday(+this.date, +this.clockIn.toDate());
+  /**
+   * Initializes the first clockIn/out of today
+   *
+   * @param clockIn
+   * @param clockOut
+   */
+  constructor(clockIn: string, clockOut?: string) {
+    this.clockIn = [this.getMomentObject(clockIn)];
+
+    if (clockOut) {
+      this.clockOut = [this.getMomentObject(clockOut)];
+    }
   }
 
+  /**
+   * Returns the first clockIn of the day
+   */
   public getClockInHour(): string {
-    return this.clockIn.format('HH:mm');
+    return this.clockIn[0].format('HH:mm');
   }
 
+  /**
+   * Returns the calculated current hours using the last clockOut - clockIn hours
+   */
   public getCurrentHours(): number {
-    return this.currentHours;
+    const index = this.clockIn.length - 1;
+
+    // When last clock out exist the current hours would be 0
+    if (this.clockOut[index]) return 0;
+
+    const clockOut = this.clockOut[index] ? +(<moment.Moment>this.clockOut[index]) : +Date.now();
+    return this.getHoursToday(clockOut, +this.clockIn[0]);
   }
 
-  private setClockInHour(clockIn: string) {
-    const hour = new Hour(clockIn);
-    this.clockIn.hours(hour.getHours());
-    this.clockIn.minutes(hour.getMinutes());
+  /**
+   * Adds a new element in the clock in/out arrays
+   * this is useful when a user does clock in/out multiple times
+   * in a day
+   *
+   * @param clockIn
+   * @param clockOut
+   */
+  public setTimeRow(clockIn: string, clockOut?: string): void {
+    this.clockIn.push(this.getMomentObject(clockIn));
+
+    if (clockOut) {
+      this.clockOut.push(this.getMomentObject(clockOut));
+    } else {
+      this.clockOut.push(null);
+    }
   }
 
-  private getHoursToday(now: number, clockIn: number): number {
-    return Number((Math.abs(now - clockIn) / 36e5).toFixed(2));
+  /**
+   * Returns a new moment object with the time of the string passed
+   *
+   * @param time The string hours to set to the time obj
+   * @param timeObj The time object reference, this must be an element from the clock in/out array
+   */
+  private getMomentObject(time: string): moment.Moment {
+    const date = moment();
+    const hour = new Hour(time);
+    date.hours(hour.getHours());
+    date.minutes(hour.getMinutes());
+
+    return date;
+  }
+
+  private getHoursToday(clockOut: number, clockIn: number): number {
+    return Number((Math.abs(clockOut - clockIn) / 36e5).toFixed(2));
   }
 }
